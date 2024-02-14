ef8a9bc2de71cc64358d3a6bb4752f8585c9bfee
daniel <daniel.s385682@cesurformacion.com>
2024-02-14 16:50:00 +0100

    Tercer commit máster

--- PruebaEGit/src/pruebasGit/PruebasGit.java
+++ PruebaEGit/src/pruebasGit/PruebasGit.java
@@ -3,7 +3,8 @@ package pruebasGit;
 public class PruebasGit {
 	public static void main(String[] args) {
 		System.out.println("Pruebas EGit");
-		System.out.println("Segundo comit maste");
+		System.out.println("Segundo commit master");
+		System.out.println("Tercer commit máster");
 	}
 
 }
